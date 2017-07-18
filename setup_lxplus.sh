c8e83859d09a50d039937c79de13e791c0aff0a7
Wen Guan <wen.guan@cern.ch>
2017-07-18 17:19:30 +0200

    move

--- setup_lxplus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-setupATLAS
-lsetup root
-source ve/bin/activate
-
-export PATH="`pwd`:${PATH}"
-export PYTHONPATH="`pwd`:${PYTHONPATH}"
-export THEANO_FLAGS="gcc.cxxflags='-march=core2'"
